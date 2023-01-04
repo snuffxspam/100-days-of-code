5c09b7afeb679b7074437f66be688e349fc753b2
snuffxspam <68016685+snuffxspam@users.noreply.github.com>
2023-01-04 13:08:00 +0300

    Update main.py

--- entryjob/main.py
+++ entryjob/main.py
@@ -4,16 +4,16 @@ import time
 
 chrome_driver_path = "C:\\Users\\ximus\\Documents\\GitHub\\chromedriver\\chromedriver.exe"
 driver = webdriver.Chrome(chrome_driver_path)
-driver.get("https://rzhev.hh.ru/account/login?backurl=%2F&hhtmFrom=main")
+driver.get("https://hh.ru/account/login?backurl=%2F&hhtmFrom=main")
 
 number = driver.find_element("name", "login")
-number.send_keys("79969238903")
+number.send_keys("login")
 
 button = driver.find_elements(By.CSS_SELECTOR, "button.bloko-link.bloko-link_pseudo")
 button[-1].click()
 
 password = driver.find_element(By.XPATH, '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div/div[1]/div[1]/div[1]/div/div[2]/form/div[2]/fieldset/input')
-password.send_keys("Bustboy1")
+password.send_keys("password")
 
 button = driver.find_element(By.XPATH, '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div/div[1]/div[1]/div[1]/div/div[2]/form/div[4]/div/button[1]')
 button.click()
