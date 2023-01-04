01e4ca5adfd54c44722c08128b8ad9a6626976b1
snuffxspam <68016685+snuffxspam@users.noreply.github.com>
2023-01-04 12:47:05 +0300

    Add files via upload

---
+++ entryjob/main.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+chrome_driver_path = "C:\\Users\\ximus\\Documents\\GitHub\\chromedriver\\chromedriver.exe"
+driver = webdriver.Chrome(chrome_driver_path)
+driver.get("https://rzhev.hh.ru/account/login?backurl=%2F&hhtmFrom=main")
+
+number = driver.find_element("name", "login")
+number.send_keys("79969238903")
+
+button = driver.find_elements(By.CSS_SELECTOR, "button.bloko-link.bloko-link_pseudo")
+button[-1].click()
+
+password = driver.find_element(By.XPATH, '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div/div[1]/div[1]/div[1]/div/div[2]/form/div[2]/fieldset/input')
+password.send_keys("Bustboy1")
+
+button = driver.find_element(By.XPATH, '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div/div[1]/div[1]/div[1]/div/div[2]/form/div[4]/div/button[1]')
+button.click()
+time.sleep(2)
+
+button = driver.find_element(By.XPATH, '//*[@id="HH-React-Root"]/div/div[2]/div[2]/div/div/div/div/form/div/div[2]/button')
+button.click()
+
+vacancy = driver.find_elements(By.CSS_SELECTOR, "a.bloko-button.bloko-button_kind-primary.bloko-button_scale-small")
+for i in vacancy:
+    i.click()
